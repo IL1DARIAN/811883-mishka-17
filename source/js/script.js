d1242a6983001d2e81be4e0a58e10445eca55fcf
IL1DARIAN <sergey_nosov1989@rambler.ru>
2019-09-03 17:14:11 +0300

    Правки по js

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,34 @@
-var basket = document.querySelector(".catalog-product__basket");
-var popup = document.querySelector(".buy-form");
+if (document.querySelector('.catalog-main')) {
+  var baskets = document.querySelectorAll(".catalog-product__basket");
+  var popupContainer = document.querySelector(".buy-form");
+  var popup = popupContainer.querySelector('.buy-form__wrapper');
 
-basket.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("buy-form--open");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("buy-form--open")) {
-      popup.classList.remove("buy-form--open");
+  const closePopup = (evt) => {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      popupContainer.classList.remove("buy-form--open");
+      window.removeEventListener('keydown', closePopup);
     }
   }
-});
+
+  const closePopupOutside = (evt) => {
+    if (!popup.contains(evt.target)) {
+      popupContainer.classList.remove("buy-form--open");
+      document.removeEventListener('click', closePopupOutside);
+    }
+  }
+
+  const openBasket = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    if (!popupContainer.classList.contains("buy-form--open")) {
+      popupContainer.classList.add("buy-form--open");
+      window.addEventListener("keydown", closePopup);
+      document.addEventListener('click', closePopupOutside);
+    }
+  }
+
+  baskets.forEach(basket => {
+    basket.addEventListener("click", openBasket);
+  });
+}
